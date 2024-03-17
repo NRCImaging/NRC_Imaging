8863874729cd3a6d9f841019a01d3f706ad110a8
AbrahamGDR <157654572+AbrahamGDR@users.noreply.github.com>
2024-03-17 12:20:47 -0600

    load extraction

---
+++ Ababo/ababo.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Load your CSV file into a DataFrame
+df = pd.read_csv("/Users/ebrahimgdr/Documents/GitHub/NRC_Imaging/Ababo/Specimen_RawData_1.csv")
+
+# Define the value of i
+i = 300  # Set your desired value of i
+
+# Find the closest value to i in the first column
+closest_value = df.iloc[(df['Time']-i).abs().argsort()[:1]].iloc[0]
+
+# Get the relevant values from the other columns
+relevant_values = closest_value[['Load', 'Extension']]
+
+print(relevant_values)
